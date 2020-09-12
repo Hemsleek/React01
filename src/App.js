f0e93519e50cad44572bb5223aa8b9ff9aa6cc07
hemsleek <asiyanbimubashir@gmail.com>
2020-09-12 15:33:30 +0100

    feat: random colors selectors

--- src/App.js
+++ src/App.js
@@ -4,17 +4,31 @@ import './App.scss'
 
 
 const App = () => {
-
-    const [state, setState] = useState("Kwara");
+    // useState
     const array=['Benue','Markurdi','Abuja']
     let rand =Math.floor(Math.random()*array.length)
+
+    const [state, setState] = useState("Kwara");
+
     const submit = () => setState(array[rand])
+
+    // random-bg
+    const [color, setColor] = useState("rgb(0,0,0)")
+
+    const RandColor = () => Math.floor(Math.random()*255);
+
+    const colors=() => {
+        setColor(`rgb(${RandColor()},${RandColor()},${RandColor()})`);
+
+        
+    }
 
     return (
         <center>
             <h1>{state}</h1> 
             <input type="text" value={state} />
             <button onClick={submit}>Change</button>
+            <div className="box" onMouseEnter={colors} style={          {backgroundColor:color}} />
         </center>
     )
 }
