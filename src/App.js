4fe4da543c80952f439d550fdeb838cf9d07d7ea
hemsleek <asiyanbimubashir@gmail.com>
2020-09-13 14:15:04 +0100

    fix: trigger

--- src/App.js
+++ src/App.js
@@ -3,14 +3,20 @@ import React, { useState } from 'react'
 import './App.scss'
 
 
-const App = () => {
+export default() => {
     // useState
-    const array=['Benue','Markurdi','Abuja']
-    let rand =Math.floor(Math.random()*array.length)
+    const array=['Benue','Markurdi','Abuja','end']
+    let rand = Math.floor(Math.random()*array.length)
+    const reRand = () =>  Math.floor(Math.random()*array.length);
+    const [state, setStates] = useState("Kwara");
 
-    const [state, setState] = useState("Kwara");
-
-    const submit = () => setState(array[rand])
+    const submit = () => {
+        if(state !== array[rand]) setStates(array[rand])
+        else {
+            rand=reRand()
+            return submit();
+        }
+    }
 
     // random-bg
     const [color, setColor] = useState("rgb(0,0,0)")
@@ -18,20 +24,17 @@ const App = () => {
     const RandColor = () => Math.floor(Math.random()*255);
 
     const colors=() => {
-        setColor(`rgb(${RandColor()},${RandColor()},${RandColor()})`);
-
-        
+        setColor(`rgb(${RandColor()},${RandColor()},${RandColor()})`);        
     }
 
     return (
         <center>
             <h1>{state}</h1> 
             <input type="text" value={state} />
-            <button onClick={submit}>Change</button>
+            <button onClick={() => submit()}>Change</button>
             <div className="box" onMouseEnter={colors} style={          {backgroundColor:color}} />
         </center>
     )
 }
 
-export default App
 
